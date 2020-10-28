85138d0c800537a9e6c0b3f7b126f0003c4c64d0
corinabejan <corinabe93@gmail.com>
2020-10-28 20:03:07 +0100

    graph is half made

--- cpu-monitor-fe/src/App.js
+++ cpu-monitor-fe/src/App.js
@@ -5,7 +5,6 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
   const { user } = useAuth0();
-  console.log(user);
 
   return (
     <div className="App">
--- cpu-monitor-fe/src/components/CpuInfo.js
+++ cpu-monitor-fe/src/components/CpuInfo.js
@@ -1,5 +1,15 @@
 import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 import axios from "axios";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  CartesianGrid,
+} from "recharts";
 
 export default function CpuInfo() {
   const [cpuInfo, setCpuInfo] = useState({});
@@ -13,23 +23,94 @@ export default function CpuInfo() {
   console.log(cpuInfo);
 
   useEffect(() => {
+    // setInterval(function() {
     cpuUsage();
+    // }, 100)
   }, []);
 
   const coresJSX = !cpuInfo.cores ? (
     "loading"
   ) : (
     <div>
-      <h4>{cpuInfo.cores[0]}</h4>
-      <h4>{cpuInfo.cores[1]}</h4>
-      <h4>{cpuInfo.cores[2]}</h4>
-      <h4>{cpuInfo.cores[3]}</h4>
-      <h4>{cpuInfo.cores[4]}</h4>
-      <h4>{cpuInfo.cores[5]}</h4>
-      <h4>{cpuInfo.cores[6]}</h4>
-      <h4>{cpuInfo.cores[7]}</h4>
+      <h4>CPU1: {cpuInfo.cores[0]}</h4>
+      <h4>CPU2: {cpuInfo.cores[1]}</h4>
+      <h4>CPU3: {cpuInfo.cores[2]}</h4>
+      <h4>CPU4: {cpuInfo.cores[3]}</h4>
+      <h4>CPU5: {cpuInfo.cores[4]}</h4>
+      <h4>CPU6: {cpuInfo.cores[5]}</h4>
+      <h4>CPU7: {cpuInfo.cores[6]}</h4>
+      <h4>CPU8: {cpuInfo.cores[7]}</h4>
     </div>
   );
+
+  const graphJSX = !cpuInfo.cores ? (
+    "loading"
+  ) : (
+    <div>
+      <LineChart
+        width={730}
+        height={250}
+        data={cpuInfo.cores}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend verticalAlign="top" height={36} />
+        <Line
+          type="monotone"
+          dataKey={cpuInfo.cores[0]}
+          stroke="red"
+          name="CPU1"
+        />
+        <Line
+          type="monotone"
+          dataKey={cpuInfo.cores[1]}
+          stroke="blue"
+          name="CPU2"
+        />
+        <Line
+          type="monotone"
+          dataKey={cpuInfo.cores[2]}
+          stroke="purple"
+          name="CPU3"
+        />
+        <Line
+          type="monotone"
+          dataKey={cpuInfo.cores[3]}
+          stroke="yellow"
+          name="CPU4"
+        />
+        <Line
+          type="monotone"
+          dataKey={cpuInfo.cores[4]}
+          stroke="orange"
+          name="CPU5"
+        />
+        <Line
+          type="monotone"
+          dataKey={cpuInfo.cores[5]}
+          stroke="green"
+          name="CPU6"
+        />
+        <Line
+          type="monotone"
+          dataKey={cpuInfo.cores[6]}
+          stroke="black"
+          name="CPU7"
+        />
+        <Line
+          type="monotone"
+          dataKey={cpuInfo.cores[7]}
+          stroke="pink"
+          name="CPU8"
+        />
+      </LineChart>
+    </div>
+  );
+
+  console.log(graphJSX)
 
   return (
     <div>
@@ -42,7 +123,15 @@ export default function CpuInfo() {
       <p>
         Average speed: <strong>{cpuInfo.avg}</strong>
       </p>
-      {coresJSX}
+      <div>{coresJSX}</div>
+      <div>{graphJSX}</div>
     </div>
   );
 }
+
+LineChart.propTypes = {
+  data: PropTypes.array.isRequired,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  margin: PropTypes.object,
+};
