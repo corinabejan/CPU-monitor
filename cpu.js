e04b73dee74fcbee476417bbc6e844c6f9dad83c
corinabejan <corinabe93@gmail.com>
2020-10-28 10:59:52 +0100

    last fixes

--- cpu.js
+++ cpu.js
@@ -1,21 +1,22 @@
 const Router = require("express");
 const router = new Router();
-const si = require('systeminformation')
+const si = require("systeminformation");
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    const cpuInfo = await si.cpuCurrentspeed(data => {
-      return data
+    const cpuInfo = await si.cpuCurrentspeed((data) => {
+      return data;
     });
-    if(!cpuInfo) {
-      res.status(404).send({message: 'There was no CPU usage data found!'})
+    if (!cpuInfo) {
+      res.status(404).send({ message: "There was no CPU usage data found!" });
     } else {
-      res.status(200).send({message: 'There was CPU usage data found', cpuInfo})
+      res
+        .status(200)
+        .send({ message: "There was CPU usage data found", cpuInfo });
     }
-  } catch(e) {
-    next(e)
+  } catch (e) {
+    next(e);
   }
-})
-
+});
 
 module.exports = router;
